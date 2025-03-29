8b61ca1a422b95cc382784284e714456ed2016e9
lauuurars <1120098177@u.icesi.edu.co>
2025-03-29 15:49:27 -0500

    fin punto 1

---
+++ Actividad 1/punto1.js
@@ -0,0 +1,10 @@
+
+//Desarrollo Actividad 1
+
+let animales = ["zorro", "gato", "perro", "león", "tigre"] //definimos un array de animales
+
+console.log(animales) //se muestra el array original
+
+animales.unshift("conejo") //agregamos un elemneto al inicio del array con el método unshift
+
+console.log(animales) //se muestra el array con el nuevo elemento al inicio
