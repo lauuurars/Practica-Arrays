dd1b5fe0db1845507ca6c39b9d79965c1f97265f
lauuurars <1120098177@u.icesi.edu.co>
2025-03-29 16:14:20 -0500

    fin punto 4

---
+++ Actividad 4/punto4.js
@@ -0,0 +1,10 @@
+
+//Desarrollo Actividad 4
+
+let numeros = [7, 15, 20, 42, 77] //definimos array de numeros
+
+console.log(numeros) //imprimimos array original
+
+numeros.splice(3,1) //eliminamos el elemento en la posicion 3 del array con el metodo splice
+
+console.log(numeros) //imprimimos el array actualizado con los cambios
