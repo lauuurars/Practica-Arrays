0fe5ba01fe7e9bd2fc3db540298e3f86d4a10ae4
lauuurars <1120098177@u.icesi.edu.co>
2025-03-29 16:07:44 -0500

    fin punto 3

---
+++ Actividad 3/punto3.js
@@ -0,0 +1,10 @@
+
+//Desarrollo Actividad 3
+
+let frutas = ["fresa", "arandanos", "banano", "cereza", "kiwi"] //definimos array de frutas
+
+console.log(frutas) //se imprime el array original
+
+let indiceCereza = frutas[3] //agregamos variable que representa el indice del elemento cereza
+
+console.log(indiceCereza) //se imprime el valor de la variable indiceCereza
