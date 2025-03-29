8b8e6c586ad8a88a61fecc11694eef5bd801b493
lauuurars <1120098177@u.icesi.edu.co>
2025-03-29 15:55:31 -0500

    fin punto 2

---
+++ Actividad 2/punto2.js
@@ -0,0 +1,10 @@
+
+//Desarrollo Actividad 2
+
+let colores = ["rojo", "lila", "azul", "naranja", "verde"] //definimos array de colores
+
+console.log(colores) //se imprime el array original
+
+colores.shift() //eliminamos el primer elemento del array con el método shift
+
+console.log(colores) //se imprime el array actualizado con los cambios
