52f7126ef7c5588dee31ebe026722f10523152b3
lauuurars <1120098177@u.icesi.edu.co>
2025-03-29 16:34:05 -0500

    fin punto 5

---
+++ Actividad 5/punto5.js
@@ -0,0 +1,10 @@
+
+//Desarrollo Activiad 5
+
+let estudiantes = ["Sofía", "Luis", "Juan", "Mario", "María"] //definimos el array estudiantes
+
+console.log(estudiantes) //imprimimos el array de estudiantes original
+
+estudiantes.splice(3,0, "Paola") //agregamos un nuevo elemento en la posición 3 del array
+
+console.log(estudiantes) //imprimimos el array de estudiantes con el nuevo elemento agregado
